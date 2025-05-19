7cc8522d902f6f66dc3061009966445848a1b25f
Jonathan Yanez <jon.yanezy@gmail.com>
2025-05-19 02:48:39 -0400

    actualizacion de modelo y vistas

--- Aplicacion/urls.py
+++ Aplicacion/urls.py
@@ -5,7 +5,4 @@ app_name = "Aplicacion"
 
 urlpatterns = [
     path('post_listar', views.post_listar, name='post_listar'),
-    #path('create-category/', views.create_category, name='create_category'),
-    #path('create-post/', views.create_post, name='create_post'),
-    #path('search/', views.search_post, name='search_post'),
 ]
--- Entrega_Final/urls.py
+++ Entrega_Final/urls.py
@@ -14,12 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+# #from Aplicacion import views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path("", include("Main.urls"), name="index"),
+#     path("Aplicacion/", include("Aplicacion.urls"), name="index")
+# ]
+
 from django.contrib import admin
 from django.urls import path, include
-#from Aplicacion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("Main.urls"), name="index"),
-    path("Aplicacion/", include("Aplicacion.urls"), name="index")
+    path("", include("Main.urls", namespace="Main")),
+    path("Aplicacion/", include("Aplicacion.urls", namespace="Aplicacion")),
 ]
